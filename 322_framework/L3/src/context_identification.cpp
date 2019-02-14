3d9ea6d9b9f5fc260a559f6b214b74ca1ac25b46
Maggie <margaretlou2019@u.northwestern.edu>
2019-02-14 12:10:14 -0600

    Context identification code

--- 322_framework/L3/src/context_identification.cpp
+++ 322_framework/L3/src/context_identification.cpp
@@ -4,3 +4,34 @@
 
 // Context object
 // list of Instructions*
+#include <L3.h>
+#include <context_identification.h>
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <iterator>
+
+using namespace std;
+
+namespace L3{
+  vector<vector<Instruction*>> generate_contexts(Function* f) {
+    vector<vector<Instruction*>> contexts;
+    vector<Instruction*> current_context;
+    for (Instruction* i : f->instructions) {
+      if (!current_context.empty() && i->type == Instruction_type::LABELI) {
+        contexts.push_back(current_context);
+        current_context.clear();
+      }
+      current_context.push_back(i);
+      if (i->type == Instruction_type::JUMP) {
+        contexts.push_back(current_context);
+        current_context.clear();
+      }
+    }
+    if (!current_context.empty()) {
+      contexts.push_back(current_context);
+    }
+    return contexts;
+  }
+}
---
+++ 322_framework/L3/src/context_identification.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <L3.h>
+
+using namespace std;
+
+namespace L3{
+
+  vector<vector<Instruction*>> generate_contexts(Program p);
+
+}
